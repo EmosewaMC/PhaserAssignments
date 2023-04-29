f6dc8c81e04deae4ed79d535fc5b31b2d96b9355
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2023-04-28 18:31:02 -0700

    Finish all scenes except menu
    Just need to make the text when I get home

--- game.js
+++ game.js
@@ -3,18 +3,46 @@ function nextScene(currentScene, nextScene) {
 	currentScene.scene.start(nextScene);
 }
 
+class PlayIntro extends Phaser.Scene {
+	constructor() {
+		super('PlayIntro');
+	}
+	preload() {
+	}
+	create() {
+		this.input.once('pointerdown', function () {
+			nextScene(this, 'StudioLogoIntro');
+		}, this);
+		this.add.text(
+			275,
+			100,
+			'Click to start',
+			{
+				font: '32px Arial',
+				fill: '#FFFFFF',
+				align: 'left',
+				stroke: '0xFFFFFF',
+				strokeThickness: 4
+			}
+		);
+	}
+}
+
+
 class StudioLogoIntro extends Phaser.Scene {
 	constructor() {
 		super('StudioLogoIntro');
 	}
 	preload() {
 		this.load.image('studio', './assets/StudioLogo.png');
+		this.load.audio('intro', './assets/ArmorGamesIntro.mp3')
 	}
 	create() {
 		this.input.once('pointerdown', function () {
 			nextScene(this, 'D1GameScene2');
 		}, this);
 		this.studioSprite = this.add.sprite(this.game.config.width / 2, this.game.config.height / 2, 'studio').setAlpha(0);
+		this.sound.play('intro');
 		this.tweens.add({
 			targets: this.studioSprite,
 			duration: 1000,
@@ -26,7 +54,7 @@ class StudioLogoIntro extends Phaser.Scene {
 			duration: 1000,
 			delay: 2750,
 			alpha: 0,
-			onComplete: function () { nextScene(this.parent.scene, 'D1GameScene2') },
+			onComplete: function () { this.parent.scene.sound.stopAll(); nextScene(this.parent.scene, 'D1GameScene2') },
 		});
 	}
 }
@@ -85,18 +113,65 @@ class D1GameScene2 extends Phaser.Scene {
 	}
 }
 
+function renderText(scene, text, x, y) {
+	scene.add.text(
+		x,
+		y,
+		text,
+		{
+			font: '64px Arial',
+			fill: '#FFFFFF',
+			align: 'center',
+			stroke: '0xFFFFFF',
+			strokeThickness: 6
+		}
+	);
+}
+
 class D1GameScene3 extends Phaser.Scene {
 	constructor() {
 		super('D1GameScene3');
 	}
 	preload() {
-		this.load.image('blueberry', 'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcSLBRJfSJABUvT7tJeDy4RGOsuQb6Li-p-D-8zYZCyqtLRhgHtItinCALqqOp-sjx85b1nJqPyBx865SqU');
+		this.load.image('blueberry', './assets/goldfish.png');
+		this.load.audio('menu', './assets/Timesplitters_2_Music_streets.mp3');
 	}
 	create() {
-		this.add.sprite(this.game.config.width / 2, this.game.config.height / 2, 'blueberry').setScale(0.5);
+		this.add.sprite(500, 350, 'blueberry').setScale(0.25);
+		renderText(this, 'Play', 25, 25); // These need to be images instead which i create in krita with TS font.
+		renderText(this, 'Options', 25, 100); // These need to be images instead which i create in krita with TS font.
+		renderText(this, 'Credits', 25, 175); // These need to be images instead which i create in krita with TS font.
+		renderText(this, 'Quit', 25, 250);
+		renderText(this, 'Extreme Fishing 3', 275, 25);
+		this.add.text(
+			275,
+			100,
+			'Press F to fish and when a \nfish is ready press G to pull up',
+			{
+				font: '32px Arial',
+				fill: '#FFFFFF',
+				align: 'left',
+				stroke: '0xFFFFFF',
+				strokeThickness: 4
+			}
+		);
+		this.sound.play('menu');
 		this.input.once('pointerdown', function () {
-			nextScene(this, 'StudioLogoIntro');
+			this.sound.stopAll();
+			nextScene(this, 'EndScene');
 		}, this);
+	}
+}
+
+class EndScene extends Phaser.Scene {
+	constructor() {
+		super('EndScene');
+	}
+	preload() {
+		this.load.image('end', './assets/EndScene.png');
+	}
+	create() {
+		this.endSprite = this.add.sprite(this.game.config.width / 2, this.game.config.height / 2, 'end');
 	}
 }
 
@@ -106,7 +181,7 @@ const config = {
 	height: 600,
 	backgroundColor: '#FFFFFF',
 	parent: 'mainGame',
-	scene: [StudioLogoIntro, D1GameScene2, D1GameScene3]
+	scene: [PlayIntro, StudioLogoIntro, D1GameScene2, D1GameScene3, EndScene]
 };
 
 const game = new Phaser.Game(config);
